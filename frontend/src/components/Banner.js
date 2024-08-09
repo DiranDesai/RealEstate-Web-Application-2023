007a5df5b52fee8f3725097962f5c92ccbc2503b
Diran sai <65699558+DiranDesai@users.noreply.github.com>
2024-08-09 12:28:51 +0200

    Update Banner.js

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -6,7 +6,7 @@ function Banner() {
     <div className="banner">
         <div className="banner-content">
             <h1>Find Your Dream Home</h1>
-            <p>From as low as $10 per day with limited time offer discounts </p>
+            <p>From as low as K600 per day with limited time offer discounts </p>
             <div className="banner-control-buttons">
                 <button className="btn buy">Buy<i className="bi bi-bag"></i></button>
                 <button className="btn rent">Rent<i className="bi bi-house-add"></i></button>
