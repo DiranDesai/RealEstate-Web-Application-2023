c2441d2a27c841896bef1917b362babe0d46c3d2
Diran sai <65699558+DiranDesai@users.noreply.github.com>
2024-08-14 08:35:37 +0200

    Update SingleProperty.js the title

--- frontend/src/components/SingleProperty.js
+++ frontend/src/components/SingleProperty.js
@@ -45,7 +45,7 @@ function SingleProperty({property}) {
         </div>
         <div className="middle">
           <Link to={`property/${_id}`}>
-            <h4 className="title">{title}</h4>
+            <h4 className="title fw-70">{title}</h4>
           </Link>
           <p><span className="cap">{address}</span> {/*<span className="time"><i class="bi bi-clock"></i>{formatAgoTime(createdAt)}</span>*/}</p>
           <div className="property-options d-flex justify-content-between align-items-center">
