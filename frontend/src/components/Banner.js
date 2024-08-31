c7c47d347d47bd5d88437e6c08edc98346454d9a
Diran sai <65699558+DiranDesai@users.noreply.github.com>
2024-08-31 06:42:26 +0200

    Update Banner.js deleting the banner-content

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -4,13 +4,6 @@ import BannerSearch from './BannerSearch'
 function Banner() {
   return (
     <div className="banner">
-        <div className="banner-content">
-            <h1>Find Your Dream Home</h1>
-            <p>From as low as K600 per Month with limited time offer discounts </p>
-            <div className="banner-control-buttons">
-                <button className="btn buy">Buy<i className="bi bi-bag"></i></button>
-                <button className="btn rent">Rent<i className="bi bi-house-add"></i></button>
-            </div>
             <BannerSearch />
         </div>
     </div>
