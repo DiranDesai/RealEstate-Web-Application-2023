5f6ed4942ac601f03e89cc3983621d0dc1128f4a
Diran sai <65699558+DiranDesai@users.noreply.github.com>
2024-09-29 14:37:51 +0200

    Update SingleProperty.js

--- frontend/src/components/SingleProperty.js
+++ frontend/src/components/SingleProperty.js
@@ -27,7 +27,7 @@ function SingleProperty({property}) {
 
   return (
     <div className="col-md-3 property" key={_id}>
-      <div className="">
+      <div className="card shadow-2">
         <div className="top">
           <div className="property-tags d-flex justify-content-between align-items-center">
             <div>
