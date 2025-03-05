2a46dbf8c1ef4a244e257642728d5b145841e7bc
DiranDesai <dirantechie@gmail.com>
2025-03-05 17:03:19 +0200

    Updating codebase

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -3,7 +3,7 @@ import React from "react";
 function Loader() {
   return (
     <div className="loader-wrapper">
-      <div className="loader"></div>
+      <div className="loader2"></div>
     </div>
   );
 }
